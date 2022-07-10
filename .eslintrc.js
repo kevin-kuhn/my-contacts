7a19cf4e1802a0f1254d9fd4c827d8d43b8b1620
Kevin Kuhn <kmulinarik@hotmail.com>
2022-07-10 17:19:05 -0300

    feat update and create methods

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,18 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
     'class-methods-use-this': 'off',
     'no-shadow': 'off',
+    camelcase: 'off',
+    'object-curly-newline': 'off',
+    'no-confusing-arrow': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'function-paren-newline': 'off',
+    'comma-dangle': 'off',
   },
 };
--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,9 +19,63 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {}
+  async store(req, res) {
+    const { name, email, phone, category_id } = req.body;
 
-  update() {}
+    if (!name || !email) {
+      res.status(400).json({ error: 'Name and email is required' });
+      return;
+    }
+
+    const contactExists = await ContactRepository.findByEmail(email);
+
+    if (contactExists) {
+      res.status(400).json({ error: 'Contact already exists' });
+      return;
+    }
+
+    const contact = await ContactRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    res.json(contact);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { name, email, phone, category_id } = req.body;
+
+    const contactExists = await ContactRepository.findById(id);
+
+    if (!contactExists) {
+      res.status(404).json({ error: 'Contact not found' });
+      return;
+    }
+
+    if (!name || !email) {
+      res.status(400).json({ error: 'Name and email is required' });
+      return;
+    }
+
+    const contactEmail = await ContactRepository.findByEmail(email);
+
+    if (contactEmail && contactEmail.id !== id) {
+      res.status(400).json({ error: 'Contact already exists' });
+      return;
+    }
+
+    const contact = await ContactRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    res.json(contact);
+  }
 
   async delete(request, response) {
     const { id } = request.params;
